d345ba7178ec2bc30eec41a7a3b7a1ffedbd2f53
Alex <alex@MacBook-Pro-Alex.local>
2023-04-29 16:56:08 +0300

    added the FillArray method

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿// Задача: Написать программу, которая из имеющегося массива 
+// строк формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется 
+// пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+int GetLenghtArray(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int lenght = GetLenghtArray("Введите количество элементов массива: ");
+
+string[] FillArray(int length)
+{
+    string[] array = new string[length];
+    for (var i = 0; i < length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
